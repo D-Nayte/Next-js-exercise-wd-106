6d1966ed253c2b8d3c90cc81826486636ea62fdc
Andy Schunke <schunke.andy@gmail.com>
2022-11-28 15:00:27 +0100

    finished

--- app/[post]/error.js
+++ app/[post]/error.js
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <p>Something went wrong!</p>
       <button onClick={() => reset()}>Reset error boundary</button>
     </div>
   );
