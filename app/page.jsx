b887dcdd05c8249b5942f63247ca1d63d1966123
Andy Schunke <schunke.andy@gmail.com>
2022-11-28 14:55:25 +0100

    finished

--- app/page.jsx
+++ app/page.jsx
@@ -4,26 +4,14 @@ import LinkToPost from "../components/LinkToPost";
 import { getAllPosts, getPosts, getPostWithId1 } from "../lib/API-Posts";
 
 async function Blogposts() {
-  const posts = await getAllPosts();
   const singlePost = await getPostWithId1();
   return (
     <ul className="posts-wrapper">
-      <li>
-        <Link href="/new_page"> New Page</Link>
-      </li>
       <li>
         <Link href="/single_post">
           <LinkToPost post={singlePost} />
         </Link>
       </li>
-      {posts &&
-        posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/post?id=${post.id}`}>
-              <LinkToPost post={post} />
-            </Link>
-          </li>
-        ))}
     </ul>
   );
 }
